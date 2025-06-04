d13a56db833a2da3cc056a1e06d92b4cd32a6710
BlissNathan1305 <sambless44@gmail.com>
2025-06-04 17:23:07 +0100

    good

---
+++ fib.py
@@ -0,0 +1,15 @@
+def fibonacci_recursive(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    seq = fibonacci_recursive(n - 1)
+    seq.append(seq[-1] + seq[-2])
+    return seq
+
+# Example usage
+num = int(input("Enter number of terms: "))
+print(f"The first {num} Fibonacci numbers are: {fibonacci_recursive(num)}")
